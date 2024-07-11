69db76c5e16f1bb8e81f377617640c96745b3164
Ziyu-Mu <mu.ziyu.ovo@gmail.com>
2024-07-11 22:19:10 +0200

    file organize
    Signed-off-by: Ziyu-Mu <mu.ziyu.ovo@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mushroom_classification",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Project for MLOps - Mushroom Classification",
+    author="C. Zhang, Y. Yao, Z. Zhong, Z. Mu",
+    license="MIT",
+)
